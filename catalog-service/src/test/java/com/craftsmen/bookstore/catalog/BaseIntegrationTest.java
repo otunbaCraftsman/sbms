663ec0666d0071d7187aabf41f1386f333684a26
Ayodele <ayodele.glow@gmail.com>
2025-04-03 16:18:47 +0100

    integration testing with rest assured and slice testing

---
+++ catalog-service/src/test/java/com/craftsmen/bookstore/catalog/BaseIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.craftsmen.bookstore.catalog;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class BaseIntegrationTest {
+
+    @LocalServerPort
+    int randomPort;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.port = randomPort;
+    }
+
+
+}
--- catalog-service/src/test/java/com/craftsmen/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/craftsmen/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class CatalogServiceApplicationTests {
+class CatalogServiceApplicationTests extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {}
+
 }
